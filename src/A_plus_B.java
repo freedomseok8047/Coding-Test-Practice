f8cc9ea0b97afe44dd4a7dd08a4888e967e08a3c
user <freedomseok@gmail.com>
2024-01-27 01:06:49 +0900

    다른 반복문 예시 추가

--- src/A_plus_B.java
+++ src/A_plus_B.java
@@ -33,3 +33,86 @@ public class A_plus_B {
     }
 
 }
+
+// while ~ if ~ break 구문
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int T = scanner.nextInt();
+//
+//        for (int testcase = 1; testcase <= T; testcase++) {
+//            System.out.print("Case_#" + testcase + ":");
+//
+//            int A;
+//            while (true) {
+//                A = scanner.nextInt();
+//                if (A >= 1 && A <= 9) {
+//                    break;
+//                }
+//            }
+//
+//            int B;
+//            while (true) {
+//                B = scanner.nextInt();
+//                if (B >= 1 && B <= 9) {
+//                    break;
+//                }
+//            }
+//
+//            int sum = A + B;
+//            System.out.println(sum);
+//        }
+//    }
+
+// for 구문 대신에 while로 반복
+// public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//
+//    int T = scanner.nextInt();
+//
+//    int testcase = 1;
+//    while (testcase <= T) {
+//        System.out.print("Case_#" + testcase + ":");
+//
+//        int A;
+//        do {
+//            A = scanner.nextInt();
+//        } while (A < 0 || A > 9);
+//
+//        int B;
+//        do {
+//            B = scanner.nextInt();
+//        } while (B < 0 || B > 9);
+//
+//        int sum = A + B;
+//        System.out.println(sum);
+//
+//        testcase++;
+//    }
+//}
+
+
+// do~while 과 그냥 while 차이점 : do~while은 최소 1번은 실행
+
+//public static void main(String[] args) {
+//    Scanner scanner = new Scanner(System.in);
+//
+//    int T = scanner.nextInt();
+//
+//    for (int testcase = 1; testcase <= T; testcase++) {
+//        System.out.print("Case_#" + testcase + ":");
+//
+//        int A = scanner.nextInt();
+//        while (A < 0 || A > 9) {
+//            A = scanner.nextInt();
+//        }
+//
+//        int B = scanner.nextInt();
+//        while (B < 0 || B > 9) {
+//            B = scanner.nextInt();
+//        }
+//
+//        int sum = A + B;
+//        System.out.println(sum);
+//    }
+//}
